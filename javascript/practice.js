fa60a911ac10d9f06ce879c3c635c564d47f94ca
Genemichael25 <genemichael25@yahoo.com>
2023-10-04 17:43:43 -0700

    branch

--- javascript/practice.js
+++ javascript/practice.js
@@ -26,3 +26,5 @@
 // Write a statement that takes a variable of a grade percentage and logs the letter grade for that percentage, if the grade is 100% log "perfect score", if the grade is zero log "no grade available."
 // Write a statement that takes a variable of a boolean, number, or string data type and logs the data type of the variable. HINT: Check out the JavaScript typeof operator.
 // Create a password checker using a single conditional statement. If a user inputs a password with 12 or more characters AND the password includes !, then log "That is a mighty strong password!" If the user’s password is 8 or more characters OR includes !, then log "That password is strong enough." Log "That is not a valid password." for every other input.
+
+//Testing
